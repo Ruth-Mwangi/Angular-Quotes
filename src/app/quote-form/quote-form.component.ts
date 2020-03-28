2fd812f7cdb8264f17a20d1dceb6dde02481e77e
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-28 17:11:45 +0300

    udated onsubmit function

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quote-form',
@@ -11,13 +12,15 @@ export class QuoteFormComponent implements OnInit {
   newQuote=new Quote(0,new Date(),"","","",0,0)
   @Output() addQuote= new EventEmitter<Quote>();
 
-  onSubmit(){
+  onSubmit(form: NgForm){
     var check=this.newQuote;
     if(check.userName===""||check.quote===""||check.author===""){
       alert("Please fill in all fields")
       return false;
     }
     this.addQuote.emit(this.newQuote);
+  
+    form.resetForm(); 
   }
 
   constructor() { }
