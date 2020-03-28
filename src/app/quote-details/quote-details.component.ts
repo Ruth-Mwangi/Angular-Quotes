2eee566ff2b5b9fc31376708ecf4404f79e470ea
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-28 18:53:14 +0300

    create quoteDeletion function

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -10,6 +10,11 @@ export class QuoteDetailsComponent implements OnInit {
 
 
   @Input() quote:Quote;
+  @Output() isDeleted=new EventEmitter<boolean>();
+
+  quoteDeletion(remove:boolean){
+    this.isDeleted.emit(remove);
+  }
 
   constructor() { }
 
