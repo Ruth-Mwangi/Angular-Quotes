109a11ff1ce358e9cbe70efe5f9881b89d1c4756
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-28 17:34:39 +0300

    added addNewGoal function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+
+  quotes:Quote[]=[];
+
+  addNewGoal(quote){
+    let quotesLength=this.quotes.length+1;
+    let up=0;
+    let down=0;
+
+    let quoteObj=new Quote(quotesLength,new Date,quote.userName,quote.author,quote.quote,up,down);
+
+    this.quotes.push(quoteObj);
+  }
 
   constructor() { }
 
