135b5569fbbdd5d373e78f2a5dd1a84d7f1d8dd7
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-28 20:15:25 +0300

    created a datesPassed pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { from } from 'rxjs';
+import { DatePassedPipe } from './date-passed.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { from } from 'rxjs';
     HighlightDirective,
     QuoteFormComponent,
     QuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    DatePassedPipe
   ],
   imports: [
     BrowserModule,
